5d05dde7ff1ee872bc302fb397348405d824800b
Kanav-ar <kanava879@gmail.com>
2026-05-25 21:05:37 +0000

    db config changes

--- src/db/index.js
+++ src/db/index.js
@@ -1,14 +1,22 @@
+import dotenv from 'dotenv'
+dotenv.config({path:"../../.env"})
 import mongooose from "mongoose";
-import { DB_NAME } from "../constants";
+import { DB_NAME } from "../constants.js";
 import mongoose from "mongoose";
+import express from "express";
+const app = express();
+
 
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
       `${process.env.MONGODB_URI}/${DB_NAME}`,
     );
+    
   } catch (error) {
     console.log("ERROR:", error);
     process.exit(1);
   }
 };
+connectDB()
+export default connectDB;
