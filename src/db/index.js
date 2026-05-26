c3253e68278f1e1b3ef872e18f5a6ca3dd2838d7
Kanav-ar <kanava879@gmail.com>
2026-05-26 08:10:33 +0000

    Server setup

--- src/db/index.js
+++ src/db/index.js
@@ -1,10 +1,5 @@
-import dotenv from 'dotenv'
-dotenv.config({path:"../../.env"})
-import mongooose from "mongoose";
 import { DB_NAME } from "../constants.js";
 import mongoose from "mongoose";
-import express from "express";
-const app = express();
 
 
 const connectDB = async () => {
@@ -12,11 +7,13 @@ const connectDB = async () => {
     const connectionInstance = await mongoose.connect(
       `${process.env.MONGODB_URI}/${DB_NAME}`,
     );
+    console.log(connectionInstance.connection.host);
+    
     
   } catch (error) {
     console.log("ERROR:", error);
     process.exit(1);
   }
 };
-connectDB()
+
 export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" });
+import connectDB from "./db";
+import express from "express";
 
 const app = express();
 
-( async () => {
-    try {
-        
-    } catch (error) {
-        console.log("ERROR:",error)
-    }
-
-})()
+connectDB()
+  .then(() => {
+    let PORT = process.env.PORT || 8000;
+    app.on("error", (error) => {
+      console.log("SERVER ERROR: ", error);
+      throw error;
+    });
+    app.listen(PORT, () => {
+      console.log("Connection successful! Listening on the port", PORT);
+    });
+  })
+  .catch((err) => {
+    console.log("Connection failed", err);
+  });
