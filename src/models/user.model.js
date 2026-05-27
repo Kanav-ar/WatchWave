9c446bd79fb6c123d7d7f74b9c26f1500b65c5cc
Kanav-ar <kanava879@gmail.com>
2026-05-27 18:05:10 +0000

    refresh token function fix

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -75,7 +75,7 @@ userSchema.methods.generateAccessToken = function () {
     { expiresIn: process.env.ACCESS_TOKEN_EXPIRY },
   );
 };
-userSchema.methods.generateAccessToken = function () {
+userSchema.methods.generateRefreshToken = function () {
   return jwt.sign(
     {
       _id: this._id,
@@ -84,5 +84,5 @@ userSchema.methods.generateAccessToken = function () {
     { expiresIn: process.env.REFRESH_TOKEN_EXPIRY },
   );
 };
-userSchema.methods.generateAccessToken = function () {};
+
 export const User = mongoose.model("User", userSchema);
