e13926cf7f7f88be39cd393f8f7b9964f730602a
Kanav-ar <kanava879@gmail.com>
2026-06-14 13:33:47 +0530

    refactor like schema and implement like controllers

--- Backend/src/models/like.model.js
+++ Backend/src/models/like.model.js
@@ -1,24 +1,57 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
-
-const likeSchema = new Schema({
+const likeSchema = new Schema(
+  {
     video: {
-        type: Schema.Types.ObjectId,
-        ref: "Video"
+      type: Schema.Types.ObjectId,
+      ref: "Video",
     },
     comment: {
-        type: Schema.Types.ObjectId,
-        ref: "Comment"
+      type: Schema.Types.ObjectId,
+      ref: "Comment",
     },
     post: {
-        type: Schema.Types.ObjectId,
-        ref: "Post"
+      type: Schema.Types.ObjectId,
+      ref: "Post",
     },
     likedBy: {
-        type: Schema.Types.ObjectId,
-        ref: "User"
+      type: Schema.Types.ObjectId,
+      ref: "User",
     },
-    
-}, {timestamps: true})
+  },
+  { timestamps: true },
+);
 
-export const Like = mongoose.model("Like", likeSchema)
+likeSchema.index(
+  { video: 1, likedBy: 1 },
+  {
+    unique: true,
+    partialFilterExpression: {
+      video: { $exists: true },
+    },
+  },
+);
+
+// Like a comment only once
+likeSchema.index(
+  { comment: 1, likedBy: 1 },
+  {
+    unique: true,
+    partialFilterExpression: {
+      comment: { $exists: true },
+    },
+  },
+);
+
+// Like a post only once
+likeSchema.index(
+  { post: 1, likedBy: 1 },
+  {
+    unique: true,
+    partialFilterExpression: {
+      post: { $exists: true },
+    },
+  },
+);
+
+export const Like = mongoose.model("Like", likeSchema);
